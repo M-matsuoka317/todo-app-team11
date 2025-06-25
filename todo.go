ae70f09f6b45faa1b2027eb01e2aa4279c995250
Masaki Matsuoka <m242710@hiroshima-u.ac.jp>
2025-06-25 10:41:23 +0900

    Implement AddTask adn ListTasks

--- todo.go
+++ todo.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"encoding/json"
 	"os"
 )
@@ -50,11 +51,33 @@ func nextID(tasks []Task) int {
 }
 
 func AddTask(title string) {
-	panic("unimplemented")
+	var tasks []Task
+	tasks, _ = loadTasks()
+	var new_id = nextID(tasks)
+	var task Task
+	task.Title = title
+	task.ID = new_id
+	task.Done = false
+	tasks = append(tasks, task)
+
+	// tasks[new_id-1] = task
+	// tasks := []Task{task}
+	// // var tasks []Task
+	saveTasks(tasks)
+	// panic("unimplemented")
 }
 
 func ListTasks() {
-	panic("unimplemented")
+	var tasks []Task
+	tasks, _ = loadTasks()
+	for _, v := range tasks{
+		if v.Done{
+			fmt.Println(v.ID, v.Title, "[x]")
+		} else {
+			fmt.Println(v.ID, v.Title, "[]")
+		}
+	}
+	// panic("unimplemented")
 }
 
 func CompleteTask(id int) {
